d35f95a78154a8c550f5d6d1a65c0653617b6e07
Emery Rodziewicz <emmsi@LAPTOP-2RRKCOC6.(none)>
2021-01-28 19:47:56 -0700

    Initial commit

---
+++ src/rodziewicz/Main.java
@@ -0,0 +1,10 @@
+package rodziewicz;
+
+public class Main {
+    public static String greeting = "Hello, my name is ";
+    public static String name = "Emery";
+    public static void main(String[] args) {
+        System.out.println(greeting + name);
+
+    }
+}
