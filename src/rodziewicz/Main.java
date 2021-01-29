63bdb675ff1e75792c651870a020c8c202e9af10
Jashua <snyderjoshua81@gmail.com>
2021-01-28 21:00:40 -0700

    Added my name

--- src/rodziewicz/Main.java
+++ src/rodziewicz/Main.java
@@ -1,10 +1,7 @@
 package rodziewicz;
 
 public class Main {
-    public static String greeting = "Hello, my name is ";
-    public static String name = "Emery";
     public static void main(String[] args) {
-        System.out.println(greeting + name);
-
+        System.out.println("Hello Emery, my name is Josh");
     }
 }
