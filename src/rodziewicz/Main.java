9252e0dc50e106a423feba70935dd1fb9a8f9d8b
Jacob Sherwood <j.sherwood1820@gmail.com>
2021-01-29 11:49:56 -0700

    Added My name.

--- src/rodziewicz/Main.java
+++ src/rodziewicz/Main.java
@@ -3,5 +3,6 @@ package rodziewicz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Emery, my name is Josh");
+        System.out.println("Hello Emery and Josh, My name is Jacob");
     }
 }
